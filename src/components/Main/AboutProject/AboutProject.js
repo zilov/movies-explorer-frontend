bec8ff65418583f9597ebeb3c647d177937e4df2
Danil Zilov <zilov.d@gmail.com>
2022-10-05 11:28:48 +0300

    feat: about-project section layout

--- src/components/Main/AboutProject/AboutProject.js
+++ src/components/Main/AboutProject/AboutProject.js
@@ -1,16 +1,19 @@
 function AboutProject() {
   return(
     <div className="about-project" name="about-project">
-      <h2 className="about-project__title">О проекте</h2>
+      <h2 className="main__section-title">О проекте</h2>
+      <div className="main__section-title-underline"></div>
       <div className="about-project__grid-table">
         <h3 className="about-project__subtitle">Дипломный проект включал 5 этапов</h3>
-        <p className="about-project__paragraph">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
         <h3 className="about-project__subtitle">На выполнение диплома ушло 5 недель</h3>
+        <p className="about-project__paragraph">Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные доработки.</p>
         <p className="about-project__paragraph">У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно защититься.</p>
-        <p className="about-project__timing-backend">1 неделя</p>
-        <p className="about-project__timing-frontend">4 недели</p>
-        <p>Backend</p>
-        <p>Frontend</p>
+      </div>
+      <div className="about-project__grid-timing">
+        <p className="about-project__timing-block about-project__timing-block_type_black">1 неделя</p>
+        <p className="about-project__timing-block about-project__timing-block_type_grey">4 недели</p>
+        <p className="about-project__timing-subtitle">Backend</p>
+        <p className="about-project__timing-subtitle">Frontend</p>
       </div>
     </div>
   )
