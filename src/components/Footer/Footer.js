949154b51e59f1e9ede80a481eb681e7549ac0b7
Danil Zilov <zilov.d@gmail.com>
2022-10-06 11:51:43 +0300

    feat: footer layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,17 @@
 function Footer() {
   return(
-    <div className="footer"></div>
+    <div className="footer">
+      <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+      <div className="footer__copyright-box">
+        <p className="footer__copyright">© 2022</p>
+        <div className="footer__links-box">
+          <a href="https://github.com/zilov/movies-explorer-frontend" className="footer__link">Яндекс Практикум</a>
+          <a href="https://github.com/zilov/movies-explorer-frontend" className="footer__link">GitHub</a>
+        </div>
+      </div>
+    </div>
   )
 }
-
+            
 export default Footer;
             
