64b967dbb204e581c37bd2e62840ac82fed293a2
Danil Zilov <zilov.d@gmail.com>
2022-10-06 10:23:19 +0300

    fix: promo title-align to center

--- src/components/Main/Promo/Promo.js
+++ src/components/Main/Promo/Promo.js
@@ -1,6 +1,6 @@
 function Promo() {
   return(
-    <div className="promo border">
+    <div className="promo">
       <h1 className="promo__title">Учебный проект студента факультета Веб-разработки.</h1>
     </div>
   )
