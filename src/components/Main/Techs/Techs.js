27911532fab46d2483dfe4223441c707e509dce8
Danil Zilov <zilov.d@gmail.com>
2022-10-06 09:50:09 +0300

    feat: techs section layout

--- src/components/Main/Techs/Techs.js
+++ src/components/Main/Techs/Techs.js
@@ -4,15 +4,15 @@ function Techs() {
       <h2 className="main__section-title">Технологии</h2>
       <div className="main__section-title-underline"></div>
       <h3 className="techs__title">7 технологий</h3>
-      <p className="techs_text"></p>
-      <ul>
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JS</li>
-        <li>React</li>
-        <li>Git</li>
-        <li>Express.js</li>
-        <li>mongoDB</li>
+      <p className="techs__text">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+      <ul className="techs__grid-table">
+        <li className="techs__grid-table-item">HTML</li>
+        <li className="techs__grid-table-item">CSS</li>
+        <li className="techs__grid-table-item">JS</li>
+        <li className="techs__grid-table-item">React</li>
+        <li className="techs__grid-table-item">Git</li>
+        <li className="techs__grid-table-item">Express.js</li>
+        <li className="techs__grid-table-item">mongoDB</li>
       </ul>
     </div>
   )
