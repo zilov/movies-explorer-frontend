1d2e4c2e6030c30aaa47419e1990eeabd5bd1d09
Danil Zilov <zilov.d@gmail.com>
2022-10-08 14:43:31 +0300

    feat: header 320 and 768 layout

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,11 +4,13 @@ import logo from "../../images/logo.svg";
 function Header() {
   return(
     <div className="header">
-      <img src={logo} alt="Лого" className="header__logo"/>
-      <div className="header__links-box">
-        <NavLink to="/signup" className="header__register-btn">Регистрация</NavLink>
-        <NavLink to="/login" className="header__login-btn">Войти</NavLink>
-      </div>
+      <div className="header__content">
+        <img src={logo} alt="Лого" className="header__logo"/>
+        <div className="header__links-box">
+          <NavLink to="/signup" className="header__register-btn">Регистрация</NavLink>
+          <NavLink to="/signin" className="header__login-btn">Войти</NavLink>
+        </div>  
+      </div>      
     </div>
   )
 }
