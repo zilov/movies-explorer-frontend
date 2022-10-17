0b4b279508d15ba267e42263d4eef33ae8b5130b
Danil Zilov <zilov.d@gmail.com>
2022-10-17 16:00:20 +0300

    feat: add search form adaptive layout

--- src/components/Movies/SearchForm/SearchForm.js
+++ src/components/Movies/SearchForm/SearchForm.js
@@ -1,6 +1,16 @@
 function SearchForm() {
   return(
-    <div className="searchform"></div>
+    <div className="search-form">
+      <input className="search-form__input" type="text" id="searchInput" maxLength="30" placeholder="Фильм"/>
+      <button className="search-form__submit-btn"></button>
+      <div className="search-form__switch">
+        <label className="search-form__switch-box" htmlFor="short-checkbox">
+          <input className="search-form__checkbox" type="checkbox" id="short-checkbox"/>
+          <div className="search-form__switch-slider"></div>
+        </label>
+        <p className="search-form__short-title">Короткометражки</p>
+      </div>
+    </div>
   )
 }
 
