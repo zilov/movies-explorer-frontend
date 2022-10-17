b111fe86bcf6c6df3b84fa705da403034d983f91
Danil Zilov <zilov.d@gmail.com>
2022-10-17 17:40:35 +0300

    fix: remove footer from signin and signup pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,18 +1,25 @@
+import { useLocation } from "react-router";
+
 function Footer() {
-  return(
-    <div className="footer">
-      <div className="footer__content">
-        <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
-        <div className="footer__copyright-box">
-          <p className="footer__copyright">© 2022</p>
-          <div className="footer__links-box">
-            <a href="https://github.com/zilov/movies-explorer-frontend" className="footer__link">Яндекс Практикум</a>
-            <a href="https://github.com/zilov/movies-explorer-frontend" className="footer__link">GitHub</a>
+  const location = useLocation();
+  if (["/signup", "/signin"].includes(location.pathname)) {
+    return null;
+  } else {
+    return(
+      <div className="footer">
+        <div className="footer__content">
+          <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+          <div className="footer__copyright-box">
+            <p className="footer__copyright">© 2022</p>
+            <div className="footer__links-box">
+              <a href="https://github.com/zilov/movies-explorer-frontend" className="footer__link">Яндекс Практикум</a>
+              <a href="https://github.com/zilov/movies-explorer-frontend" className="footer__link">GitHub</a>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
             
 export default Footer;
