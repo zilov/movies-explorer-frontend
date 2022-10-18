3bc893c18c7388c9392041f6ebfda6ba87510a59
Danil Zilov <zilov.d@gmail.com>
2022-10-18 19:37:31 +0300

    feat: profile page adaptive layout

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,17 @@
 function Profile() {
   return(
     <div className="profile">
-      <p>Страница профиля</p>
+      <h2 className="profile__header">Привет, Виталий!</h2>
+      <div className="profile__info-block">
+        <h3 className="profile__info-key">Имя</h3>
+        <h3 className="profile__info-value">Виталий</h3>
+      </div>
+      <div className="profile__info-block">
+        <h3 className="profile__info-key">E-mail</h3>
+        <h3 className="profile__info-value">pochta@pochta.ru</h3>
+      </div>
+      <button className="profile__edit-btn">Редактировать</button>
+      <button className="profile__logout-btn">Выйти из аккаунта</button>
     </div>
   )
 }
