e4190174dd73b01efe1e4167bf29291732becd67
Danil Zilov <zilov.d@gmail.com>
2022-10-19 23:06:39 +0300

    feat: navigation menu adaptive layout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,32 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import accountIcon from "../../images/account-icon.svg";
 
 function Navigation() {
-  return(
-    <div className="navigation">
-      <NavLink to='/signup'>Регистрация</NavLink>
-      <NavLink to='/signin'>Войти</NavLink>
-      <NavLink to='/'>Главная</NavLink>
-      <NavLink to='/movies'>Фильмы</NavLink>
-      <NavLink to='/saved-movies'>Сохраненные фильмы</NavLink>
-      <NavLink to='/profile'>Профиль</NavLink>
-    </div>
-  )
+  const location = useLocation();
+  if (["/movies", "/saved-movies", "/profile"].includes(location.pathname)) {
+    const is_movies = location.pathname === "/movies";
+    const is_saved = location.pathname === "/saved-movies"
+    return(
+      <nav className="navigation">
+        <div className="navigation__content">
+          <input type="checkbox" className='navigation__menu-btn-checkbox'/>
+          <button className='navigation__menu-btn'/>
+          <li className="navigation__menu">
+            <NavLink to='/' className="navigation__menu-link">Главная</NavLink>
+            <NavLink to='/movies' className={`navigation__menu-link ${is_movies && "navigation__menu-link_underline"}`}>Фильмы</NavLink>
+            <NavLink to='/saved-movies' className={`navigation__menu-link ${is_saved && "navigation__menu-link_underline"}`}>Сохраненные фильмы</NavLink>
+            <NavLink to='/profile' className="navigation__menu-link">
+              <p>Аккаунт</p>
+              <img src={accountIcon} alt="иконка профиля" className="navigation__profile-icon"/>
+            </NavLink>
+          </li>
+          <div className="navigation__menu-opacity"/>
+        </div>
+      </nav>
+      )
+  } else {
+    return null;
+  }
 }
 
 export default Navigation;
