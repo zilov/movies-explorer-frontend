ff3e1513d2221b2d24481a2cdca7e6942948d216
Danil Zilov <zilov.d@gmail.com>
2022-10-20 12:42:37 +0300

    fix: about-me link opacity and target _blank

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -11,7 +11,7 @@ function AboutMe() {
           <p className="about-me__subtitle about-me__grid-table-first-box">Фронтенд-разработчик, 25 лет</p>
           <p className="about-me__info about-me__grid-table-first-box">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена 
   и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
-          <a className="about-me__link about-me__grid-table-first-box" href="https://github.com/zilov">GitHub</a>
+          <a className="about-me__link about-me__grid-table-first-box link-opacity" href="https://github.com/zilov" target={"_blank"} rel="noreferrer">GitHub</a>
           <img className="about-me__photo" src={avatar} alt="фото для портфолио"/>
         </div>
       </div>
