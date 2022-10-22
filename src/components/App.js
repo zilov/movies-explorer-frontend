d56f555386ac5a47f577e01b483eb71a893db56a
Danil Zilov <zilov.d@gmail.com>
2022-10-22 16:13:30 +0300

    fix: move app pages to main

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,16 @@
 import './App.css';
 import Header from './Header/Header';
-import Login from './Login/Login';
-import Register from './Register/Register';
 import Main from './Main/Main';
-import Profile from './Profile/Profile';
-import Movies from './Movies/Movies';
-import SavedMovies from './SavedMovies/SavedMovies';
 import Footer from './Footer/Footer';
-import { Route, Switch } from 'react-router-dom';
-import NotFound from './NotFound/NotFound';
-import Navigation from './Navigation/Navigation';
+import { useLocation } from 'react-router';
 
 function App() {
-  const location = window.location.pathname;
+  const location = useLocation().pathname;
   return (
     <div className="app">
       <Header location={location}/>
-      <Navigation location={location}/>
-      <Switch>
-        <Route path="/signup">
-          <Register></Register>
-        </Route>
-        <Route path="/signin">
-          <Login></Login>
-        </Route>
-        <Route exact path="/">
-          <Main></Main>
-        </Route>
-        <Route path="/movies">
-          <Movies></Movies>
-        </Route>
-        <Route path="/saved-movies">
-          <SavedMovies></SavedMovies>
-        </Route>
-        <Route path="/profile">
-          <Profile></Profile>
-        </Route>
-        <Route path='*'>
-          <NotFound></NotFound>
-        </Route>
-      </Switch>
-      <Footer location={location}></Footer>
+      <Main location={location}/>
+      <Footer location={location}/>
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,17 +4,39 @@ import NavTab from "./NavTab/NavTab";
 import Portfolio from "./Portfolio/Portfolio";
 import Promo from "./Promo/Promo";
 import Techs from "./Techs/Techs";
+import Login from '../Login/Login';
+import Register from '../Register/Register';
+import Profile from '../Profile/Profile';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import { Route, Routes } from 'react-router-dom';
+import NotFound from '../NotFound/NotFound';
+import Navigation from "../Navigation/Navigation";
 
-function Main() {
+
+function Main({location}) {
   return(
-    <div className="main">
-      <Promo className="main__section-content"/>
-      <NavTab className="main__section-content"/>
-      <AboutProject className="main__section-content"/>
-      <Techs className="main__section-content"/>
-      <AboutMe className="main__section-content"/>
-      <Portfolio className="main__section-content"/>
-    </div>
+    <section className="main-block">
+      <Navigation location={location}/>
+      <Routes>
+        <Route path="/signup" element={<Register/>}/>
+        <Route path="/signin" element={<Login/>}/>
+        <Route exact path="/" element={
+          <section className="main-page">
+            <Promo className="main__section-content"/>
+            <NavTab className="main__section-content"/>
+            <AboutProject className="main__section-content"/>
+            <Techs className="main__section-content"/>
+            <AboutMe className="main__section-content"/>
+            <Portfolio className="main__section-content"/>
+          </section>
+        }/>
+        <Route path="/movies" element={<Movies/>}/>
+        <Route path="/saved-movies" element={<SavedMovies/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </section>
   )
 }
 
