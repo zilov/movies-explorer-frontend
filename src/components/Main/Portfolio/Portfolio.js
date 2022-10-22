c9a5312d65299ef1c31c45c7de0e1d151a2ff67e
Danil Zilov <zilov.d@gmail.com>
2022-10-22 17:42:34 +0300

    add links to github-pages

--- src/components/Main/Portfolio/Portfolio.js
+++ src/components/Main/Portfolio/Portfolio.js
@@ -4,16 +4,16 @@ function Portfolio() {
       <p className="portfolio__title">Портфолио</p>
       <ul className="portfolio__grid-table">
         <li className="portfolio__grid-table-item">
-          <a className="portfolio__link link-opacity" href="https://github.com/zilov/how-to-learn" target="_blank" rel="noreferrer">Статичный сайт</a>
-          <a href="https://github.com/zilov/how-to-learn" className="portfolio__link-icon link-opacity" target="_blank" rel="noreferrer">↗</a>
+          <a className="portfolio__link link-opacity" href="https://zilov.github.io/how-to-learn/" target="_blank" rel="noreferrer">Статичный сайт</a>
+          <a href="https://zilov.github.io/how-to-learn/" className="portfolio__link-icon link-opacity" target="_blank" rel="noreferrer">↗</a>
         </li>
         <li className="portfolio__grid-table-item">
-          <a className="portfolio__link link-opacity" href="https://github.com/zilov/russian-travel" target="_blank" rel="noreferrer">Адаптивный сайт</a>
-          <a href="https://github.com/zilov/russian-travel" className="portfolio__link-icon link-opacity" target="_blank" rel="noreferrer">↗</a>
+          <a className="portfolio__link link-opacity" href="https://zilov.github.io/russian-travel/" target="_blank" rel="noreferrer">Адаптивный сайт</a>
+          <a href="https://zilov.github.io/russian-travel/" className="portfolio__link-icon link-opacity" target="_blank" rel="noreferrer">↗</a>
         </li>
         <li className="portfolio__grid-table-item">
-          <a className="portfolio__link link-opacity" href="https://github.com/zilov/react-mesto-auth" target="_blank" rel="noreferrer">Одностраничное приложение</a>
-          <a href="https://github.com/zilov/react-mesto-auth" className="portfolio__link-icon link-opacity" target="_blank" rel="noreferrer">↗</a>
+          <a className="portfolio__link link-opacity" href="https://github.com/zilov/react-mesto-api-full" target="_blank" rel="noreferrer">Одностраничное приложение</a>
+          <a href="https://github.com/zilov/react-mesto-api-full" className="portfolio__link-icon link-opacity" target="_blank" rel="noreferrer">↗</a>
         </li>
       </ul>
     </section>
