936c4b106a356a085307217727322cb690652ca2
Danil Zilov <zilov.d@gmail.com>
2022-10-25 18:45:38 +0300

    feat: add inputs validators

--- src/components/FormSection/FormSection.js
+++ src/components/FormSection/FormSection.js
@@ -1,9 +1,20 @@
-function FormSection({header, id}) {
+function FormSection({header, inputSettings}) {
+
+  const handleInput = (e) => {
+    inputSettings.setValue(e.target.value);
+  }
+
   return(
     <div className="form-section">
       <h3 className="form-section__header">{header}</h3>
-      <input className="form-section__input" id={id}/>
-      <span className="form-section__err" id={`${id}-Error`}></span>
+      <input className="form-section__input"
+        id={inputSettings.id}
+        type={inputSettings.type}
+        minLength={inputSettings.minLength || "0"}
+        maxLength={inputSettings.maxLength || "9999"}
+        onChange={handleInput}
+      />
+      <span className="form-section__err" id={`${inputSettings.id}-Error`}></span>
     </div>
   )
 }
