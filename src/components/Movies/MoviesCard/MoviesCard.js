a955688c3b392a495044dd8ea595b9c93bd82d26
Danil Zilov <zilov.d@gmail.com>
2022-10-28 16:56:47 +0300

    feat: card component features

--- src/components/Movies/MoviesCard/MoviesCard.js
+++ src/components/Movies/MoviesCard/MoviesCard.js
@@ -1,15 +1,13 @@
-import card_default_img from "../../../images/card_default_img.jpg";
-
-function MoviesCard({cardType="search"}) {
+function MoviesCard({cardType="search", title, duration, imageUrl}) {
   return(
     <article className="movies-card">
       <div className="movies-card__info-box">
-        <h2 className="movies-card__title">33 слова о дизайне</h2>
-        <p className="movies-card__duration">1ч 47м</p>
+        <h2 className="movies-card__title">{title}</h2>
+        <p className="movies-card__duration">{duration}</p>
       </div>
       <input type="checkbox" className="movies-card__checkbox"/>
       <button type="button" className={`movies-card__btn ${cardType === "search" ? "movies-card__btn_type_favorite" : "movies_card__btn_type_delete"} button-opacity`}/>
-      <img className="movies-card__image" alt="обложка фильма" src={card_default_img}/>
+      <img src={imageUrl} className="movies-card__image" alt="обложка фильма"/>
     </article>
   )
 }
