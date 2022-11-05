8c27d20e038fb6d3d42b8bfa2fcaa56a6034acc1
Danil Zilov <zilov.d@gmail.com>
2022-11-05 18:43:02 +0300

    fix: header color change on main page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,7 +4,9 @@ import logo from "../../images/logo.svg";
 function Header({location}) {
   if (["/movies", "/saved-movies", "/profile", '/'].includes(location)) {
     return(
-      <header className="header header_type_white">
+      <header className={
+        `header 
+        ${location !== "/" && "header_type_white"}`}>
         <div className="header__content">
           <NavLink to="/">
             <img src={logo} alt="Лого" className="header__logo"/>
